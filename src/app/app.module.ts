f50f4276632984bb1a4f8428d51dcd767baf5b4b
Salim <bouzidsezlim@gmail.com>
2023-06-29 12:06:45 +0200

    counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoucleComponent } from './boucle/boucle.component';
-
+import { CounterComponent } from './counter/counter.component';
+import { FormsModule } from '@angular/forms';
+import { CalculComponent } from './calcul/calcul.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BoucleComponent
+    BoucleComponent,
+    CounterComponent,
+    CalculComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calcul/calcul.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calcul',
+  templateUrl: './calcul.component.html',
+  styleUrls: ['./calcul.component.css']
+})
+export class CalculComponent {
+
+}
